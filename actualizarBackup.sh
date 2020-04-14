ea43d40b9527a4bec8488a319d32bce4889bd70b
alienx456 <estebanelias27@gmail.com>
2020-04-14 20:44:05 +0000

    Agregado script para actualizar backups

---
+++ actualizarBackup.sh
@@ -0,0 +1,6 @@
+cp -r /opt/dspace/config/modules/rdf .
+cp -r /opt/dspace/config/controlled-vocabularies/ .
+cp /opt/dspace/config/dspace.cfg .
+cp /opt/dspace/config/local.cfg .
+cp /opt/tomcat/webapps/jspui/WEB-INF/classes/Messages.properties .
+cp /opt/dspace/config/input-forms.xml .
